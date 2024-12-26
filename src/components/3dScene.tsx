207b222e5c280e53014db362a610fe96b9f47626
NagatoCute <1659103717@qq.com>
2024-12-26 10:32:11 +0800

    Modify 3D scene components added wooden floor, added simple display frame.

--- src/components/3dScene.tsx
+++ src/components/3dScene.tsx
@@ -1,46 +1,94 @@
 import { useRef, useEffect, useState } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
 import { useDrag } from '@use-gesture/react'
 import * as THREE from "three";
 import { Subject, map, filter, pipe, scan, merge } from 'rxjs';
 
 export default function Scene() {
   return (
-    <Canvas
-      camera={{  // 默認值
-        position: [0, 10, 30],
-        fov: 75,
-        near: 0.1,
-        far: 1000,
-      }}
-    >
-      {/* 光照 */}
-      <ambientLight color={0x606060} intensity={1} />
-      <directionalLight color={0xbcd2ee} position={[1, 0.75, 0.5]} intensity={1} />
+      <Canvas
+          camera={{  // 默認值
+            position: [0, 10, 30],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+      >
+        {/* 光照 */}
+        <ambientLight color={0x606060} intensity={1}/>
+        <directionalLight color={0xbcd2ee} position={[1, 0.75, 0.5]} intensity={1}/>
 
-      {/* 方塊 */}
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color={0xffff00} />
-      </mesh>
+        {/* 方塊 */}
+        <mesh>
+          <boxGeometry/>
+          <meshStandardMaterial color={0xffff00}/>
+        </mesh>
 
-      <gridHelper args={[100, 20, 0xff0000, 0x808080]} /> {/* 地面 */}
-      <axesHelper args={[30]} /> {/* 坐标轴 */}
-      {/* 牆 */}
-      <gridHelper args={[100, 20, 0x00ff00, 0x808080]} position={[-50, 50, 0]} rotation={[0, 0, Math.PI / 2]} />
-      <gridHelper args={[100, 20, 0x00ff00, 0x808080]} position={[50, 50, 0]} rotation={[0, 0, Math.PI / 2]} />
+        {/*木地板*/}
+        <WoodFloor />
 
-      <CameraPosition  />
-      <CameraRotation />
-    </Canvas>
+        <gridHelper args={[100, 20, 0xff0000, 0x808080]}/>
+        {/* 地面 */}
+        <axesHelper args={[30]}/>
+        {/* 坐标轴 */}
+        {/* 牆 */}
+        <gridHelper args={[100, 20, 0x00ff00, 0x808080]} position={[-50, 50, 0]} rotation={[0, 0, Math.PI / 2]}/>
+        <gridHelper args={[100, 20, 0x00ff00, 0x808080]} position={[50, 50, 0]} rotation={[0, 0, Math.PI / 2]}/>
+
+        {/* 画框（大概） */}
+        {/* <FrameBox position={[50, 5, 0]} rotationAngle={0} /> */}
+        <FrameBox position={[-50, 11, -12]} rotationAngle={0}/>
+        <FrameBox position={[-50, 11, 12]} rotationAngle={0}/>
+        <FrameBox position={[-50, 11, 36]} rotationAngle={0}/>
+
+        <CameraPosition />
+        <CameraRotation />
+      </Canvas>
   );
+}
+
+//木地板
+function WoodFloor() {
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load('/texture/old-plank-flooring1_basecolor.png');
+
+// 设置纹理重复方式和重复次数
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set(10, 10); // 调整重复次数
+
+return (
+    <mesh position={[0, -1, 0]}>
+      <boxGeometry args={[100, 2, 100]}/>
+      <meshStandardMaterial map={texture}/>
+    </mesh>
+);
+}
+//画框占位符
+function FrameBox({position, rotationAngle}: { position: [number, number, number], rotationAngle: number }) {
+  const width = 20;
+  const height = 12;
+  const depth = 0.1;
+  // 定义图片的宽度和高度
+  const imageWidth = 1024;
+  const imageHeight = 680;
+
+  // 加载图片纹理
+  const texture = useLoader(THREE.TextureLoader, `https://placehold.co/${imageWidth}x${imageHeight}/jpg`);
+
+  return (
+      <mesh position={position} rotation={[0, rotationAngle + Math.PI / 2, 0]}>
+        <boxGeometry args={[width, height, depth]}/>
+        <meshStandardMaterial map={texture}/>
+      </mesh>
+);
 }
 
 
 function CameraPosition() {
   const direction = useKeyboardMoving();
   const speed = 30;
-  useFrame(({ camera }, delta, frame) => {
+  useFrame(({camera}, delta, frame) => {
     let [front, side] = direction; // 归一化
     let currentPos = camera.position.clone();
 
@@ -66,13 +114,18 @@ function CameraPosition() {
     // 前进后退
     currentPos.add(forward.multiplyScalar(front * speed * delta));
 
+    // 限制x轴范围在-50到50之间
+    currentPos.x = Math.max(-49, Math.min(49, currentPos.x));
+
     // 左右移动
     currentPos.add(right.multiplyScalar(side * speed * delta));
 
     // 防止飞到天上或地下
     currentPos.y = Math.max(6, 7);
 
+
     camera.position.set(currentPos.x, currentPos.y, currentPos.z);
+
   });
   return <></>;
 }
