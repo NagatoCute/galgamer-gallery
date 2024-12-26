2883e1229fb5c298daf5ceabc2df65b4bd60f13c
NagatoCute <1659103717@qq.com>
2024-12-26 23:07:00 +0800

    Modify 3D scene,add the white wall

--- src/3d-components/3dScene.tsx
+++ src/3d-components/3dScene.tsx
@@ -17,16 +17,16 @@ export default function Scene() {
       >
           {/* 光照 */}
           {/*后面的数越大阴影越淡*/}
-          <ambientLight color={0x606060} intensity={3}/>
+          <ambientLight color={0x606060} intensity={0.5}/>
           {/*<directionalLight color={0xbcd2ee} position={[1, 0.75, 0.5]} intensity={2} />*/}
           {/*方向光，投射阴影的，我调不好，但是确实可以用*/}
           <directionalLight
-              color={0xbcd2ee}
-              position={[20, 30, 20]} // 调整光源位置
-              intensity={2}
+              color={0xffffff}
+              position={[20, 20, 20]} // 调整光源位置
+              intensity={3}
               castShadow
-              shadow-mapSize-width={1024}
-              shadow-mapSize-height={1024}
+              shadow-mapSize-width={2048}
+              shadow-mapSize-height={2048}
               shadow-camera-near={0.1}
               shadow-camera-far={200}
               shadow-camera-left={-50}
@@ -36,11 +36,14 @@ export default function Scene() {
           />
 
           {/* 方塊 */}
-          <mesh>
-              <boxGeometry/>
+          <mesh position={[20, 20, 20]}>
+              <boxGeometry args={[1, 1, 1]}/>
               <meshStandardMaterial color={0xffff00}/>
           </mesh>
 
+          {/*墙面*/}
+          <WhiteWall position={[51, 50, 0]}/>
+          <WhiteWall position={[-51, 50, 0]}/>
 
           <MainBody position={[10, 1, 10]} rotationAngle={0}/>
           <TopCover position={[10, 1.5, 10]} rotationAngle={0}/>
@@ -81,13 +84,37 @@ function WoodFloor() {
     );
 
     return (
-        <mesh position={[0, -1, 0]} receiveShadow>
-            <boxGeometry args={[100, 2, 100]} />
-            <meshStandardMaterial map={texture} color={0xF5DEB3} emissive={0xD2B48C } emissiveIntensity={0.1} />
-        </mesh>
+        <>
+            <mesh position={[0, -1, 0]} receiveShadow>
+                <boxGeometry args={[100, 2, 100]}/>
+                <meshStandardMaterial
+                    map={texture}
+                    color={0xFAEBD7}
+                />
+            </mesh>
+        </>
     );
 }
 
+function WhiteWall({position,}: { position: [number, number, number]}) {
+    const texture = useTexture(
+        '/texture/old-plank-flooring1_roughness.png',
+        (texture) => {
+            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+            texture.repeat.set(10, 10);
+        }
+    );
+    return (
+        <>
+            <ambientLight intensity={0.7} />
+            {/* 添加环境光 */}
+            <mesh position={position} >
+                <boxGeometry args={[2, 100, 100]} />
+                <meshStandardMaterial map={texture} color={0xffffff}/>
+            </mesh>
+        </>
+    );
+}
 
 //画框占位符
 function FrameBox({position, rotationAngle}: { position: [number, number, number], rotationAngle: number }) {
